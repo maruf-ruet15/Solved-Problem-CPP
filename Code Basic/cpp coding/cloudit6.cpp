f38de95be2733f49d2267086306ae871cf76e289
Md. Maruf Hossain <maruf.ruet15@gmail.com>
2023-02-19 11:07:29 +0600

    cloud it code added

--- Code Basic/cpp coding/cloudit6.cpp
+++ Code Basic/cpp coding/cloudit6.cpp
@@ -14,3 +14,14 @@ for (i=0;i=-1;i=1)
 
 return 0;
 }
+
+
+// First, the variable i is initialized to 1.
+
+// Then, the for loop condition is i=-1, which assigns the value -1 to i and also evaluates to -1. This means that the loop will continue as long as the condition is true.
+
+// Inside the loop, the value of i is printed, which will be -1 in the first iteration.
+
+// Then, the condition if (i!=1) break; is evaluated. Since i is equal to -1, which is not equal to 1, the loop will exit and the program will end.
+
+// Therefore, the output of the program will be: -1
