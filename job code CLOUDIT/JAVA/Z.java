743b9207425313f2907f5ff9f1975b69f9ab182e
Md. Maruf Hossain <maruf.ruet15@gmail.com>
2023-02-23 00:25:23 +0600

    new code added

---
+++ job code CLOUDIT/JAVA/Z.java
@@ -0,0 +1,24 @@
+class Z {
+    public:
+
+// constructor
+    Z() {
+        cout << "Constructor called" << endl;
+    }
+// destructor
+
+    ~Z() {
+        cout << "Destructor called" << endl;
+    }
+};
+
+int main() {
+
+    Z zl; // Constructor Called int a = 1;
+    int i = 1;
+    if (i == 1) {
+        Z z2; // Constructor Called
+
+    } // Destructor Called for z2
+
+}// Destructor called for zl
